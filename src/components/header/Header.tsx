24d3d8077ebfb1b9b2d6518083f4967125b0f84a
Brenno Boechat <BrennoBoechat81@hotmail.com>
2023-06-26 16:35:56 -0300

    icon input

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { HeaderContainer } from './styles'
 import ProfileIcon from "../../assets/profile-user.svg"
+import SearchIcon from "../../assets/search-icon.svg"
 import Cart from "../../assets/cart.svg"
 import Image from 'next/image'
 
 function Header() {
   return (
     <HeaderContainer>
-        <div className='header-itens'>
             <h1>Cactus Company</h1>
-            <input placeholder='Buscar'></input>
+
+            <div className='input'>
+              <input placeholder='Buscar'></input>
+              <button> <Image alt='buscar' src={SearchIcon}/> </button>
+            </div>
+
             <div className='header-images'>
                 <Image alt='conta' src={ProfileIcon}/>
                 <Image alt='carrinho' src={Cart}/>
             </div>
-        </div>
     </HeaderContainer>
   )
 }
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -3,33 +3,41 @@ import styled from "styled-components"
 export const HeaderContainer = styled.div `
     display: flex;
     background-color: #E0164F;
-    width: 100vw;
-    height: 10vh;
-
-    .header-itens {
-        display: flex;
-        width: 100%;
-        align-items: center;
-        justify-content: center;
-        gap: 300px;
+    width: 100%;
+    align-items: center;
+    height: 6.6rem;
+    justify-content: space-evenly;
 
         h1 {
             color: white;
             font-size: 1.8rem;
         }
 
-        input {
+        .input {
+            display: flex;
+            background-color: white;
+            width: 400px;
             border-radius: 15px;
-            height: 30px;
-            width: 370px;
+
+            input {
+            border-radius: 15px;
+            height: 38px;
+            width: 100%;
             border: none;
-            padding: 15px;
+            padding: 10px;
+        }
+
+            button {
+                border: none;
+                border-radius: 15px;
+                background-color: white;
+                padding: 5px;
+            }
+
         }
 
     .header-images {
         display: flex;
         gap: 50px;
-    }
-
     }
 `
